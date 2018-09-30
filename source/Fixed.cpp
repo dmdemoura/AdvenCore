b21c0dc64773c3b8edc9a4f0ddcc9a16bc356792
Gabriel <gdeperes@gmail.com>
2018-09-30 01:43:28 -0300

    AdvenCore has been separated from Adven

--- source/Fixed.cpp
+++
@@ -1,9 +0,0 @@
-#include "Fixed.hpp"
-/*
-Fixed::Fixed(){}
-
-Fixed operator+(Fixed a, Fixed b)
-{
-    return Fixed(a.number + b.number);
-}
-*/
--- source/Moveable.cpp
+++
@@ -1,8 +0,0 @@
-#include "Moveable.hpp"
-
-void Moveable::Start() {}
-void Moveable::VDrawUpdate()
-{
-    gameObject->localPosition += speed;
-}
-void Moveable::VBlankUpdate() {}
